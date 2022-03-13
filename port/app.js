25589ea9854e1d68d3fee9b3d5f40d6b29763dd1
Sarthak <sarthakkaze@gmail.com>
2022-03-13 22:54:22 +0530

    tweaked colors

---
+++ port/app.js
@@ -0,0 +1,15 @@
+
+/*
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset<1100){
+        document.body.style.background = "white";
+        document.getElementById('skilltag').style.color = "black";
+    }
+    else if(window.pageYOffset>1100){
+        document.body.style.background = "rgb(53, 53, 53)";
+        document.body.style.transition = "1.5s";
+        document.getElementById('skilltag').style.color = "orange";
+      
+    }
+});
+*/
