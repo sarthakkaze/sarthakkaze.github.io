73740ebae3df434d5493fcafae7e7401ee1f22e3
Sarthak <sarthakkaze@gmail.com>
2022-03-24 20:10:20 +0530

    skills added

--- port/app.js
+++ port/app.js
@@ -13,3 +13,22 @@ window.addEventListener('scroll',function(){
     }
 });
 */
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark-theme.css";
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "light-theme.css";
+  }
+});
+//https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
